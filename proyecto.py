5d56843d4bb64628d4ce013f9f1b94d810e2f662
anzambrano21 <lolepico1234@gmail.com>
2024-03-05 14:10:30 -0400

    ddd

--- proyecto.py
+++ proyecto.py
@@ -8,7 +8,7 @@ des=[]
 oferta={}
 demanda={}
 cotoE={}
-
+hungalo=[]
 
 def crearOri(ori):
     while (True):
@@ -48,6 +48,24 @@ def transporte(destino,origen,costo_envio):
         if v.varValue > 0:
             print(v.name, "=", v.varValue)
     print('El costo mínimo es:', value(prob.objective))
+def aplicar_metodo_hungaro(cost_matrix):
+    # Crear instancia del algoritmo de asignación óptima de Munkres
+    m = Munkres()
+
+    # Aplicar el algoritmo de asignación óptima en la matriz de costos
+    asignacion = m.compute(cost_matrix)
+
+    # Obtener las asignaciones óptimas y los costos totales
+    asignacion_optima = []
+    costo_total = 0
+
+    for row, col in asignacion:
+        costo = cost_matrix[row][col]
+        asignacion_optima.append((row, col))
+        costo_total += costo
+
+    # Devolver las asignaciones óptimas y el costo total
+    return asignacion_optima, costo_total
         
 
 
